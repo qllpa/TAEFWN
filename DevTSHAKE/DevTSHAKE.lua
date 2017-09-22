8a51486ad740e0d400e0659c74fe0e0f43ac50d9
qllpa <31424247+qllpa@users.noreply.github.com>
2017-09-22 16:43:24 +0300

    Update DevTSHAKE.lua

--- DevTSHAKE/DevTSHAKE.lua
+++ DevTSHAKE/DevTSHAKE.lua
@@ -249,7 +249,7 @@ function create_config( )
     "super-ar",
     "warnall",
     },
-    sudo_users = {67369633},--Sudo users
+    sudo_users = {221117424},--Sudo users
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
     help_text_realm = [[
